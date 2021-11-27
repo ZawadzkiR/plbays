5858db98ff7450a3de39b76e0459dce9b20a9ccf
ZawadzkiR <72023108+ZawadzkiR@users.noreply.github.com>
2021-11-27 23:32:56 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,21 +1,28 @@
-import setuptools
+from setuptools import setup, find_packages
 
-with open("README.md", "r", encoding="utf-8") as fh:
-    long_description = fh.read()
+classifiers = [
+    'Development Status :: 5 - Production/Stable',
+    'Intended Audience :: Education',
+    'Operating System :: Microsoft :: Windows :: Windows 10',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3'
+]
 
-setuptools.setup(
+
+setup(
     name='plbdays',
     version='0.0.1',
     author='Robert Zawadzki',
     author_email='r.zawadzki96@gmail.com',
     description='Business days in Poland',
-    long_description=long_description,
+    long_description=open('README.txt').read() + '\n\n' + open('CHANGELOG.txt').read(),
     long_description_content_type="text/markdown",
     url='https://github.com/ZawadzkiR/plbays/',
     project_urls = {
         "Bug Tracker": "https://github.com/ZawadzkiR/plbays/issues"
     },
-    license='MIT',
-    packages=['plbdays'],
+    License='MIT',
+    packages=find_packages(),
     install_requires=['datetime'],
+    keywords = 'business days'
 )
