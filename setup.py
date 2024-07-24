1200fdebf13fbb9fe36aae7761324d5a75c5a94b
ZawadzkiR <72023108+ZawadzkiR@users.noreply.github.com>
2024-07-25 00:58:27 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ classifiers = [
 
 setup(
     name='plbdays',
-    version='1.1.0',
+    version='1.1.1',
     author='Robert Zawadzki',
     author_email='r.zawadzki96@gmail.com',
     description='Business days in Poland',
